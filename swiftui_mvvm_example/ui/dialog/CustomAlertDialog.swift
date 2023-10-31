873416be5bb6a5772e79ef332c284a5ac6e0cfbb
olehkyrhan <oleh.kyrhan@gmail.com>
2023-10-31 19:20:46 +0200

    ref CustomAlertDialog

--- swiftui_mvvm_example/ui/dialog/CustomAlertDialog.swift
+++ swiftui_mvvm_example/ui/dialog/CustomAlertDialog.swift
@@ -3,10 +3,10 @@ import UIKit
 class CustomAlertDialog {
     
     static func showInfo(_ message: String) {
-        let alert = UIAlertController(title: "Info", message: message, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
-        UIApplication.shared.rootNavigationViewController?.present(alert, animated: true, completion: nil)
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Info", message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            UIApplication.shared.rootNavigationViewController?.present(alert, animated: true, completion: nil)
+        }
     }
 }
-
-
