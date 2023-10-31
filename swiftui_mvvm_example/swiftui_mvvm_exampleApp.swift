30a3af719ad695a1a33cd5e769d56f5733276ea5
olehkyrhan <oleh.kyrhan@gmail.com>
2023-10-31 09:27:30 +0200

    Initial Commit

---
+++ swiftui_mvvm_example/swiftui_mvvm_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvm_exampleApp.swift
+//  swiftui_mvvm_example
+//
+//  Created by Oleh Kyrhan on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvm_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
