53efdbf4acccd5aa49d4c8b7f24fdf590424f4b0
olehkyrhan <oleh.kyrhan@gmail.com>
2024-04-25 09:23:50 +0300

    Commit

--- swiftui_mvvm_example/core/ui/BaseScreen.swift
+++ swiftui_mvvm_example/core/ui/BaseScreen.swift
@@ -19,3 +19,32 @@ extension BaseScreen {
         return vc!
     }
 }
+
+// MARK: inject
+extension BaseScreen {
+    
+    internal func injectViewModel<VM: BaseViewModel & StateViewModelProtocol, ST: BaseState>(_ state: ST) -> VM {
+        var vm: VM = injectViewModel()
+        if vm.state == nil {
+            vm.state = state as? VM.STATE
+        }
+        return vm
+    }
+    
+    internal func injectViewModel<VM: BaseViewModel>() -> VM {
+        if let vm: VM = viewController.getViewModel() {
+            return vm
+        }
+        let vm: VM = getViewModel()
+        viewController.addViewModel(vm)
+        return vm
+    }
+    
+    private func getViewModel<VM: BaseViewModel>() -> VM {
+        let instance = appContainer.resolve(VM.self)
+        if instance == nil {
+            fatalError("\(VM.self) nil state")
+        }
+        return instance!
+    }
+}
